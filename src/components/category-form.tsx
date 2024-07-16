0a3a3541dee8cbb9b1003360de55a2d06475b8da
ShreyaK0504 <shreyakanase38@gmail.com>
2024-07-16 18:14:12 +0530

    okay

--- src/components/category-form.tsx
+++ src/components/category-form.tsx
@@ -1,10 +1,7 @@
 'use client'
 
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select"
-import { Button } from "@/components/ui/button"
+
 import { useState } from "react"
 import axios from "axios"
 
